525f078e51a3f12fcba900c07da5d3e2cee73ecf
ayu-mushi <ayu.mushi2015@gmail.com>
2019-08-09 20:29:05 +0900

    Initial commit.

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	// Update is called once per frame
+	void Update () {
+    float x = Input.GetAxis("Horizontal");
+    float z = Input.GetAxis("Vertical");
+
+    Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+    // xとzに10をかけて押す力をアップ
+    rigidbody.AddForce(x * 10, 0, z * 10);
+	}
+}
