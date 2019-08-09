49fe1aee21fd0e5da4ffe84c92dbf29815e3121d
ayu-mushi <ayu.mushi2015@gmail.com>
2019-08-09 21:44:36 +0900

    made forest.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour {
 
     Rigidbody rigidbody = GetComponent<Rigidbody>();
 
+    Debug.Log(x);
     // xとzに10をかけて押す力をアップ
     rigidbody.AddForce(x * 10, 0, z * 10);
 	}
